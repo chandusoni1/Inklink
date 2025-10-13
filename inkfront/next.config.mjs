945a35c3419fc59f3d9edf13e2b9145549040ade
chandusoni1 <chandreshsoni8055@gmail.com>
2025-10-14 00:17:03 +0530

    config next.mjs

--- inkfront/next.config.mjs
+++ inkfront/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://inklink-zdt4.onrender.com/api/:path*", // replace with actual backend URL
+      },
+    ];
+  },
+};
 
 export default nextConfig;
